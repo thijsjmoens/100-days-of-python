7c655f7274de6adc70031312ead649be13c222eb
Thijs Moens <thijs@raketwetenschap.com>
2024-02-02 20:15:39 +0100

    Update with day 1 exercises

---
+++ day 1/input.py
@@ -0,0 +1,4 @@
+name1 = input("What is your name? ")
+print(name1) #This line will print the text and line 1 of the input into the output.
+name2 = input()
+print(name2) #This line will only print the name on line 2 of the input pane.
---
+++ day 1/variables.py
@@ -0,0 +1,17 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+b_temp = a
+a_temp = b
+
+b = b_temp
+a = a_temp
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
