c9f69140d8a60b95de003d391fbe14f6ba48e947
Thijs Moens <thijs@raketwetenschap.com>
2024-02-05 14:46:11 +0100

    Day 4 practice

---
+++ day 4/heads_or_tails.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+
+# import random module
+import random
+
+# Create a random number 0 or 1
+zero_or_one = random.randint(0, 1)
+
+if zero_or_one == 1:
+    print("Heads")
+else:
+    print("Tails")
