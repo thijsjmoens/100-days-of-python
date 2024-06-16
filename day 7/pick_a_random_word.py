c14eb43462869cbbdce43e599195042dcbc0d63f
Thijs Moens <github@moens.email>
2024-06-16 08:48:28 +0200

    Day 7

--- day 7/pick_a_random_word.py
+++ day 7/pick_a_random_word.py
@@ -1,15 +1,18 @@
 import random
 
 #Step 1 
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["ardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: \n")
+guess = input("Guess a letter: ")
 guess.lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for i in range(0, len(word_list) - 1):
-    
+for letter in chosen_word:
+    if letter == guess :
+        print("Right")
+    else:
+        print("Wrong")
