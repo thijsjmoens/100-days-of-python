9167532a43a595d8382bf4240363963e95057063
Thijs Moens <thijs.willem.moens@gmail.com>
2024-10-20 10:48:23 +0200

    Add day 10

---
+++ day 10/functions_with_parameters.py
@@ -0,0 +1,11 @@
+def formate_name(f_name, l_name):
+
+	# Concate first and last name and make sure they capitilized properly
+	full_name = f_name.title() + " " + l_name.title()
+
+	# Print the name
+	print(full_name)
+
+
+# Call the function
+formate_name("tHiJs", "moENs")
