22625ff5c235f486a11c86acd87d1d1f3522ad4c
Thijs Moens <thijs.willem.moens@gmail.com>
2024-10-20 11:13:28 +0200

    Create leap year

---
+++ day 10/leap_year.py
@@ -0,0 +1,39 @@
+def is_leap_year(year):
+
+	# Create variable for output on leap year
+	check_leap_year = False
+
+	# Check if year is divisible by 4 with no remainder
+	if year % 4:
+
+		# except every year that is evenly divisible by 100 with no remainder
+		if year % 100:
+
+			# unless the year is also divisible by 400 with no remainder
+			if year % 400: 
+
+				# It's a leap year
+				check_leap_year = True
+
+			else :
+
+				# no leap year
+				check_leap_year = False
+
+		else :
+			
+			# no leap year
+			check_leap_year = False
+
+	else :
+		
+		# no leap year
+		check_leap_year = False
+
+
+	# Return check
+	print( check_leap_year)
+
+
+# Call the function
+is_leap_year(2000)
