bf373aa73127ed0ddc6cd1002783aa228f9b8eeb
Thijs Moens <thijsmoens@email.com>
2025-01-17 12:29:00 +0100

    Update to day 14

---
+++ day 14/higher_lower_game.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Import local and external libraries
+import art
+import game_data
+import os
+
+# Credits
+__author__ = "Thijs Moens"
+__copyright__ = "Copyright 2025, Thijs Moens"
+__credits__ = ["Thijs Moens"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Thijs Moens"
+__email__ = "thijsmoens@email.com"
+__status__ = "Production"
+
+# Clear the screen
+os.system('cls||clear')
+    
+# Print the logo of the game
+print(art.logo)
+
+# Create a variable for the score
+score = ''
+
+# Laat eronder compare A zien
+
+# Laat VS art zien
+
+# Laat compare B zien
+
+# Laat de zin zien met een input
+# print("Who has more followers? Type 'A' or 'B': ")
+
+# Als het antwoord fout is
+# print(f"Sorry, that's wrong. Final score: {score}")
+
+    # Exit game
+
+# Als het antwoord goed is
+# print(f"You're right! Current score: {score}")
+
+    # Laat nieuwe comparising zien
+    
+# Retrieve the data from local lib
+data = game_data.data
+
+print(data['name'])
