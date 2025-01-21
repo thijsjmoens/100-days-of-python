f1e62de580ef2e158c83eb34dfff07d29c98225c
Thijs Moens <thijsmoens@email.com>
2025-01-21 11:31:43 +0100

    OOP exercise for day 16

--- day 16/main.py
+++ day 16/main.py
@@ -1,10 +1,21 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color('green')
-timmy.forward(100)
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color('green')
+# timmy.forward(100)
 
 
-my_screen = Screen()
-my_screen.exitonclick()
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Chespin", "Quilladin", "Chesnaught"])
+table.add_column("Type", ["Grass", "Water", "Grass"])
+
+table.align["Pokemon Name"] = "l"
+
+print(table)
