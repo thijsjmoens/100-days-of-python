fe5683ced0985dd43bffc909919c2d312971d920
Thijs Moens <thijsmoens@email.com>
2025-01-23 11:26:15 +0100

    Add day 18

---
+++ day 18/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+# Change the shape
+timmy_the_turtle.shape("turtle")
+
+# Change the color
+timmy_the_turtle.color("green")
+
+# Move it forward
+timmy_the_turtle.forward(45)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+
+screen.exitonclick()
