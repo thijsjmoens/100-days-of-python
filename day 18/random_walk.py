3433f6f74dedaf50dc156b132c6b00563d4f161a
Thijs Moens <thijsmoens@email.com>
2025-01-24 10:42:55 +0100

    Setup for random walk exercises

---
+++ day 18/random_walk.py
@@ -0,0 +1,17 @@
+''' Draw a random walk with the use of the Turtle Graphic Class
+    https://en.wikipedia.org/wiki/Random_walk
+    
+    The exercise is:
+    1. Draw a continuous random walk
+    2. Make the line each thicker everytime
+    3. Change the color of the line 
+    4. Speed up the walk
+'''
+
+# Import modules
+from turtle import Turtle, Screen
+from random import *
+
+
+
+
